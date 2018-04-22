30432b3497d5e68b182c8a949d7b31ce445e422c
ZQB-666 <15240846395@163.com>
2018-04-22 14:09:14 +0800

    首次

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by khj on 2018/4/22.
+ */
+$(".header_top .one").click(function(){
+ $(".header-left").slideToggle();
+ });
+
+
+
+//       $(".header_top .one").click(function(){
+//           $(".header-header").animate({"margin-left":"0"},1000)
+//       });
+//       $(".zfy").click(function(){
+//           $(".header-header").animate({"margin-left":"-10rem"},1000)
+//       });
